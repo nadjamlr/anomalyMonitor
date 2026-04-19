02b4c7ca7931b11f71b39426617bd997b2888a0d
Nadja Müller <n.mueller2@hm.edu>
2026-04-19 10:45:54 +0200

    Adjust Routing and folder structure

--- anomalyMonitor-app/app/_layout.tsx
+++ anomalyMonitor-app/app/_layout.tsx
@@ -5,7 +5,7 @@ import { Stack } from 'expo-router';
 export default function Layout() {
     return (
         // <AnomalyProvider>
-        <Stack>
+        <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
         // </AnomalyProvider>
