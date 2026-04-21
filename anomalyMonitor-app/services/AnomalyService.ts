1a0fc562f60c935a1fd6fbfc929b64e5aebfb4a3
Nadja Müller <n.mueller2@hm.edu>
2026-04-21 09:37:14 +0200

    Implement AnomalyService to fetch data

---
+++ anomalyMonitor-app/services/AnomalyService.ts
@@ -0,0 +1,32 @@
+// Author: Nadja Müller
+// Anomaly Data Fetch via Nasa API
+
+const API_NASA_URL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+
+
+// Anomaly Type
+interface Anomaly {
+  date: string;
+  explanation: string;
+  hdurl?: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string
+}
+
+
+// Fetch Anonmalies
+export async function fetchAnomalies(): Promise<Anomaly[]> {
+    const response = await fetch(API_NASA_URL)
+    const data = await response.json()
+    return data
+}
+
+
+const anomalyService = {
+  fetchAnomalies,
+};
+
+
+export default anomalyService;
