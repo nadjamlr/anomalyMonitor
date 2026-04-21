773d7c03579e35615453651e4c3bd2a6e4b28348
Nadja Müller <n.mueller2@hm.edu>
2026-04-21 12:24:17 +0200

    Improve SearchBar component without styling

--- anomalyMonitor-app/components/SearchBar.tsx
+++ anomalyMonitor-app/components/SearchBar.tsx
@@ -1,30 +1,37 @@
 import { globalStyles } from "../constants/styles";
 import { Colors } from '../constants/colors'
 import { Text, TextInput, View, StyleSheet} from "react-native";
+import { useAnomaly } from "../context/AnomalyContext";
 
 
 interface SearchBarProps {
     value: string;
+    searchValue: () => void;
 }
 
 
-export default function SearchBar({value}: SearchBarProps) {
+export default function SearchBar({value, searchValue}: SearchBarProps) {
     return(
-        <TextInput
-            style={styles.searchbar}
-            placeholder="Search"
-            placeholderTextColor={Colors.primary01}
-            value={value}
-            onChangeText={}
-        />
-    )
+        <View style={styles.wrapper}>
+            <TextInput
+                style={styles.input}
+                placeholder="Search"
+                placeholderTextColor={Colors.primary01}
+                value={value}
+                onChangeText={searchValue}
+            />
+        </View>
 
+    )
 }
 
 const styles = StyleSheet.create ({
-    searchbar: {
+    wrapper: {
         width: '100%',
         backgroundColor: Colors.primary02,
         color: Colors.primary01
     },
+    input: {
+        width: '100%',
+    }
 })
